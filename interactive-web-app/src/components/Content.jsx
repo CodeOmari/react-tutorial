7125c5a883dfdcc5c9fd13a027855bb3377666a9
CodeOmari <jacksonomari2230@gmail.com>
2025-05-28 15:46:32 +0300

    submission handling

--- interactive-web-app/src/components/Content.jsx
+++ interactive-web-app/src/components/Content.jsx
@@ -1,15 +1,36 @@
+import React from "react"
+
 export default function Content(){
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsList = ingredients.map(ingredient => (
+        <li key={ingredient}> {ingredient} </li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get('ingredient')
+
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
     return(
         <div className="form">
-            <form action="#">
+            <form onSubmit={handleSubmit}>
                 {/* 
                     aria-label provides alternative text for elements that do not have visible text on the
                     screen, making them accessible to users using assistive technologies e.g. screen readers
                 */}
 
-                <input type="text" placeholder="e.g. Pepper " aria-label="add ingredient" className="text-box" />
+                <input type="text" placeholder="e.g. Pepper " aria-label="add ingredient" className="text-box" name="ingredient" />
 
                 <button className="add-btn">&#43; Add ingredient</button>
+
+                <ul className="list">
+                    {ingredientsList}
+                </ul>
             </form>
         </div>
     )
