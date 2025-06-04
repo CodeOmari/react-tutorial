6804d27c53dca0db33b5e04c04a814f0e4952ff6
CodeOmari <jacksonomari2230@gmail.com>
2025-06-04 12:11:24 +0300

    conditional rendering in React

--- conditional-rendering/src/content.jsx
+++ conditional-rendering/src/content.jsx
@@ -1,18 +1,36 @@
 import React from "react"
 
-// Allows you to display differrent UI elements based on specific conditions
+// Allows you to display different UI elements based on specific conditions
 // This makes an aplication more responsive and interactive
 export default function Content(){
 
-    const [unreadMessages, setUnreadMessages] = React.useState(["a", "b", "c"])
+    const [unreadMessages, setUnreadMessages] = React.useState([])
+
+    // Using conditional rendering with ternary
+
+    function messageChecker(){
+        if (unreadMessages.length === 0){
+            return "You're all caught up!"
+        } else if(unreadMessages.length === 1){
+            return 'You have 1 unread message'
+        } else{
+            return `You have ${unreadMessages.length} unread messages`
+        }
+    }
 
     return(
         <div>
-            {
-                unreadMessages.length > 0 && 
-
-                <h1>You have {unreadMessages.length} unread messages!</h1>
-            }
+            <h1>{messageChecker()}</h1>
         </div>
+
+
+        // Using conditional rendering with &&
+        // <div>
+        // {
+        //     unreadMessages.length > 0 && 
+
+        //     <h1>You have {unreadMessages.length} unread messages!</h1>
+        // }
+        // </div>
     )
 }
